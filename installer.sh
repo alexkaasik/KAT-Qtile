427180a01ee84fdd674681a3f04d81203c9768ff
alexkaasik <70683933+alexkaasik@users.noreply.github.com>
2022-07-29 23:13:47 +0000

    Added a installer script

---
+++ installer.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+#echo "A user sudo password need to be enter:" 
+
+read -r -p "A user sudo password need to be enter:" password
+
+cd ~/
+
+git clone https://github.com/vinceliuice/Matcha-gtk-theme.git
+git clone https://github.com/ryanoasis/nerd-fonts.git
+git clone https://github.com/nonpop/xkblayout-state.git
+
+sudo -S <<< $password Matcha-gtk-theme/install.sh
+make -C xkblayout-state/
+sudo -S <<< $password cp xkblayout-state/xkblayout-state /bin/
+sudo -S <<< $password nerd-fonts/install.sh
+
+Distro=$(cat /etc/*-release | grep "^ID=")
+Distro=$(echo "$Distro" | sed -r 's/[ID=]+//g')
+
+for ((i = 0 ; i <= 1; i++)); do
+        case $Distro in
+        	arch)
+                xargs sudo -S <<< $password pacman -Sy --needed --noconfirm < packageArch
+				break
+				;;
+        	fedora)
+                xargs sudo -S <<< $password dnf -y install < packageFedora
+				break
+				;;
+        	debian)
+                xargs sudo -S <<< $password apt-get -y install < packageDeb
+				break
+				;;
+			ubuntu)
+                xargs sudo -S <<< $password apt-get -y install < packageDeb
+			    break
+			    ;;
+        	*)
+			    echo "$Distro was based on"
+			    Distro=$(cat /etc/*-release | grep "^ID_LIKE=")
+			    Distro=$(echo "$Distro" | sed -r 's/[ID_LIKE=]+//g')
+			    ;;
+        esac
+done
+
+mv KAT-Qtile/.* .
+
+rm -rf Matcha-gtk-theme nerd-fonts xkblayout-state KAT-Qtile 
+
+sudo -S <<< $password reboot
