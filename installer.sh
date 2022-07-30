698c58221201a450aa37a7e2a23c5933475d640f
alexkaasik <70683933+alexkaasik@users.noreply.github.com>
2022-07-31 00:04:28 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,35 +4,33 @@
 
 read -r -p "A user sudo password need to be enter:" password
 
-cd ~/
-
 Distro=$(cat /etc/*-release | grep "^ID=")
 Distro=$(echo "$Distro" | sed -r 's/[ID=]+//g')
 
 for ((i = 0 ; i <= 1; i++)); do
-        case $Distro in
-        	arch)
-                	xargs sudo -S <<< $password pacman -Sy --needed --noconfirm < packageArch
+	case $Distro in
+		arch)
+			xargs sudo -S <<< $password pacman -Sy --needed --noconfirm < packageArch
 			break
 			;;
-        	fedora)
-                	xargs sudo -S <<< $password dnf -y install < packageFedora
+		fedora)
+			xargs sudo -S <<< $password dnf -y install < packageFedora
 			break
 			;;
-        	debian)
-                	xargs sudo -S <<< $password apt-get -y install < packageDeb
+		debian)
+			xargs sudo -S <<< $password apt-get -y install < packageDeb
 			break
 			;;
 		ubuntu)
 			xargs sudo -S <<< $password apt-get -y install < packageDeb
 			break
 			;;
-        	*)
+		*)
 			echo "$Distro was based on"
 			Distro=$(cat /etc/*-release | grep "^ID_LIKE=")
 			Distro=$(echo "$Distro" | sed -r 's/[ID_LIKE=]+//g')
 			;;
-        esac
+	esac
 done
 
 git clone https://github.com/vinceliuice/Matcha-gtk-theme.git
@@ -43,3 +41,8 @@ sudo -S <<< $password Matcha-gtk-theme/install.sh
 make -C xkblayout-state/
 sudo -S <<< $password cp xkblayout-state/xkblayout-state /bin/
 sudo -S <<< $password nerd-fonts/install.sh
+
+
+mv ~/KAT-Qtile/.* ~/
+
+rm -rf Matcha-gtk-theme nerd-fonts xkblayout-state KAT-Qtile && sudo -S <<< $password reboot
